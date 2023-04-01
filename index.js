d1ae2153f486fae263f08b369eaec5521773f359
lordish <gitonga403@gmail.com>
2023-04-01 14:08:24 +0300

    Displayed content on Card

--- index.js
+++ index.js
@@ -1,54 +1,39 @@
-
 const nowPlayingUl = document.getElementById('now-playing')
 
-// function show (){
-//     fetch ('http://localhost:3000/films')
-//     .then(function(response){
-//         return response.json()
-//     })
-//     .then(function(data){
-//         console.log(data)
-//     })
-// }
-// show();
-
-function showMovies (){
+function showMovies() {
     fetch('http://localhost:3000/films')
-    .then(function(response){
-         return response.json()
-    })
-    .then(function(data){
-        const movies = data;
-        let moviesArr = movies.map(movie =>{
-            let movieObj = {
-                title : movie.title,
-                runtime : movie.runtime,
-                showtime : movie.showtime,
-                poster : movie.poster,
-                description : movie.description
-            }
-            return movieObj
-        }
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(data) {
+            const movies = data;
+            let moviesArr = movies.map(movie => {
+                let movieObj = {
+                    title: movie.title,
+                    runtime: movie.runtime,
+                    showtime: movie.showtime,
+                    poster: movie.poster,
+                    description: movie.description
+                }
+                return movieObj
+            })
 
-        )
-        //console.log(moviesArr);
-        moviesArr.map(displayMovies)
-        console.log(moviesArr[0])
-    })
+            moviesArr.map(movie => displayMovies(movie))
+        })
 }
-//showMovies()
-function displayMovies() {
-    let HTMLTemplate = `
-    <li >
-      <div class="card" style="width: 18rem;">
+
+function displayMovies(movieObj) {
+    let HTMLTemplate = `<li class="mx-6" >
+      <div class="card my-4" style="width: 150px;">
          <img src="${movieObj.poster}" class="card-img-top" alt="${movieObj.title}">
         <div class="card-body">
         <small class="card-title"><a class="title" href="#">${movieObj.title}</a></small>
-        <p class="card-text">${movieObj.description}</p>
+         <p class="card-text">${movieObj.description}</p>
         </div>
         </div>
     </li>
     `
-     return nowPlayingUl.innerHTML+=HTMLTemplate;
+    return nowPlayingUl.innerHTML += HTMLTemplate;
 }
- showMovies()
+
+showMovies()
