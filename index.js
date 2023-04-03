d3533ced810c6dfb8f2d70c5df5562d3b6e5ba4c
lordish <gitonga403@gmail.com>
2023-04-03 09:08:38 +0300

    Added a styling bit

--- index.js
+++ index.js
@@ -23,17 +23,27 @@ function showMovies() {
 }
 
 function displayMovies(movieObj) {
-    let HTMLTemplate = `<li class="mx-6" >
-      <div class="card my-4" style="width: 150px;">
+    let HTMLTemplate = `<li class="col-md-3">
+      <div class="card my-4" style="width: 300px;">
          <img src="${movieObj.poster}" class="card-img-top" alt="${movieObj.title}">
         <div class="card-body">
-        <small class="card-title"><a class="title" href="#">${movieObj.title}</a></small>
-         <p class="card-text">${movieObj.description}</p>
+        <medium class="card-title"><a class="title" href="#">${movieObj.title}</a></medium>
+        <p class="card-text"></p>
         </div>
         </div>
     </li>
     `
     return nowPlayingUl.innerHTML += HTMLTemplate;
 }
+
+// nowPlayingUl.addEventListener('click', displayContent);
+
+// function displayContent(event){
+//     let clickedMovie = event.target.closest('.card')
+//     let moviePoster = clickedMovie.querySelector('.card-img-top').getAttribute('src')
+//     let movieRunTime = clickedMovie.querySelector('.runtime').textContent;
+//     let details = `${moviePoster} + ${movieRunTime}`
+//     console.log(details);
+// }
 
 showMovies()
